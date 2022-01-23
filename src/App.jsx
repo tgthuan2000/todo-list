1145e3d733bae32cc7914b6b1a1f8fa8b789046c
tgthuan <tgthuan2000@gmail.com>
2022-01-23 12:58:19 +0700

    ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,68 @@
+import { useState } from 'react';
 import './app.css';
-import Box from './Components/Box';
 import Input from './Components/Input';
+import Text from './Components/Text';
 
 function App() {
+	const [isShow, setIsShow] = useState(false);
+	const [todos, setTodos] = useState([
+		{ id: Math.random(), val: 'thom' },
+		{ id: Math.random(), val: 'thuan' },
+	]);
+	const [newTodos, setNewTodos] = useState();
+
+	const handleSaveClick = () => {
+		setTodos(newTodos.filter((i) => i.val.trim() !== ''));
+		setIsShow(!isShow);
+	};
+
+	const handleShowClick = () => {
+		setIsShow(!isShow);
+		setNewTodos(todos);
+	};
+
+	const handleAddClick = () => {
+		setNewTodos([...newTodos, { id: Math.random(), val: '' }]);
+	};
+
 	return (
 		<div className='root-container'>
-			<Box />
-			<Input />
+			<button className='btn' onClick={handleShowClick}>
+				{!isShow ? 'Thêm hoặc chỉnh sửa' : 'Huỷ'}
+			</button>
+			<div className='wraper'>
+				{!isShow ? (
+					<div className='wrap'>
+						{/* Chế độ hiển thị */}
+						{todos.map(({ id, val }) => (
+							<Text key={id}>{val}</Text>
+						))}
+					</div>
+				) : (
+					<>
+						{/* Ché độ chỉnh sửa */}
+						<button
+							className='btn'
+							style={{ marginBottom: 5 }}
+							onClick={handleAddClick}>
+							+
+						</button>
+						<div className='wrap'>
+							{newTodos.map((item) => (
+								<Input
+									key={item.id}
+									{...item}
+									onChange={setNewTodos}
+									todos={newTodos}
+								/>
+							))}
+						</div>
+						<button className='btn' style={{ width: '100%' }} onClick={handleSaveClick}>
+							Lưu
+						</button>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
---
+++ src/Components/Input.jsx
@@ -0,0 +1,24 @@
+const Input = ({ id, val, onChange, todos }) => {
+	const handleChange = (e) => {
+		const index = todos.findIndex((x) => x.id === id);
+		if (index !== -1)
+			onChange(
+				todos.map((x, i) => {
+					if (index === i) x.val = e.target.value.trim();
+					return x;
+				})
+			);
+	};
+
+	return (
+		<input
+			placeholder='enter text'
+			autoFocus
+			value={val}
+			onChange={handleChange}
+			className='input'
+		/>
+	);
+};
+
+export default Input;
